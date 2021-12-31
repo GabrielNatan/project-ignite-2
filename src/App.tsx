3a6976b0d7b3b1b7da025d6d2aa2028e58023151
Gabriel Natan <gabriel--natan@live.com>
2021-12-30 21:39:33 -0300

    ajustes dash

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {BrowserRouter as  Router } from 'react-router-dom';
+import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <Router>
+      <GlobalStyle/>
+          <Routes/>
+      </Router>
+    </>
   );
 }
 
